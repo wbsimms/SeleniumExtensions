11fcf98614260a488f9062ddc9c15cbe82580854
Wm. Barrett Simms <barrett@wbsimms.com>
2015-12-30 15:43:23 -0600

    First pass

---
+++ src/SeleniumExtensions/WebElementExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumExtensions
+{
+	public static class WebElementExtensions
+	{
+		public static WebDriverWait SendKeysAndWait(this IWebElement element, IWebDriver driver, string content, int seconds = 3)
+		{
+			element.SendKeys(content);
+			return new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+		}
+
+		public static WebDriverWait ClickAndWait(this IWebElement element, IWebDriver driver, int seconds = 3)
+		{
+			element.Click();
+			return new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+		}
+
+	}
+}
